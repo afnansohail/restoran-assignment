8c8c2a7acea9dd662ad41eaf53f43fbea6a83c32
afnansohail <afnansohail1999@gmail.com>
2024-11-07 12:10:21 +0500

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-window.addEventListener("scroll", function () {
+function handleScroll() {
   const nav = document.querySelector(".nav");
   if (window.scrollY > 100) {
     nav.classList.add("scrolled");
   } else {
     nav.classList.remove("scrolled");
   }
-});
+}
+
+window.addEventListener("scroll", handleScroll);
+window.addEventListener("load", handleScroll);
